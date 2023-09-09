92b71eee37694f367217157b7222fbd038bd0c8b
Nyhilo <jacclowa@gmail.com>
2023-09-08 23:30:44 -0700

    Implement non-relational player properties (with getter/setters)

--- apeiros/game.py
+++ apeiros/game.py
@@ -1,9 +1,26 @@
 from .database import Database
+from .models import Player, Medal
 
 
 class Game():
     def __init__(self, connectionString: str = 'sqlite+pysqlite:///:memory:') -> None:
         self.db = Database(connectionString)
 
-    def test():
-        print('Test string.')
+    def test(self):
+        # Create some dummy models
+        
+
+
+        nyhilo = Player(
+            unique_id='00000016570670912',
+            username='nyhilo',
+            nickname='Nyhilo',
+            medals=['participation_medal'],
+            points=0,
+            items={'treats': 10, 'moments': 20}
+        )
+
+        self.db.add_player(nyhilo)
+
+        p = self.db.get_player('00000016570670912')
+        print(p)
--- apeiros/models/player.py
+++ apeiros/models/player.py
@@ -1,8 +1,9 @@
-from typing import Optional
+from typing import Optional, List, Dict, Any
 
-from sqlalchemy import Integer, String, Binary
+from sqlalchemy import BLOB
 from sqlalchemy.orm import Mapped, mapped_column
-from .database import Base
+
+from .base import Base
 
 
 class Player(Base):
@@ -20,19 +21,55 @@ class Player(Base):
     nickname: Mapped[Optional[str]]
 
     # A png byte blob
-    player_token: Mapped[Optional[Binary]]
+    player_token = mapped_column(type_=BLOB, nullable=True)
 
-    # Medals owned by this player
-    # TODO: This needs a relationship with a Medal class, or just a byteflag set if these are fungible
-    # medals: Mapped[]
+    # Medal ids owned by this player. Is stored as a comma-separated list of ids
+    _medals: Mapped[str] = mapped_column('medals')
+
+    @property
+    def medals(self) -> List[str]:
+        return self._medals.split(',')
+
+    @medals.setter
+    def medals(self, medals) -> None:
+        # Sanitize ids for characters that are used in storage
+        for medal in medals:
+            if ',' in medal:
+                raise ValueError('Detected comma in medals list. Couldn\'t store list in database.')
+
+        self._medals = ','.join(medals)
 
     # Currency, in the form of points.
     points: Mapped[int]
 
-    # Items owned by this player. Individual items inherit from the Item class
-    # TODO: This needs a relationship with an Item class, or just an id list if those are fungible
-    # items: Mapped[]
+    # Items owned by this player. This is a dictionary with item_ids as keys and the amount owned as values
+    _items: Mapped[str] = mapped_column('items')
+
+    @property
+    def items(self) -> Dict[str, int]:
+        pairs = self._items.split(',')
+        split_pairs = [pair.split(':') for pair in pairs]
+
+        return {pair[0]: int(pair[1]) for pair in split_pairs}
+
+    @items.setter
+    def items(self, items):
+        # Sanitize ids for characters that are used in storage
+        for key in items:
+            if ',' in key:
+                raise ValueError(f'Detected comma in items key "{key}". Couldn\'t store dictionary in database.')
+
+            if ':' in key:
+                raise ValueError(f'Detected colon in items key "{key}". Couldn\'t store dictionary in database.')
+
+        self._items = ','.join(f'{k}:{v}' for k, v in items.items())
 
     # The nomsters owned by this player
     # TODO: This needs a relationship with a Nomster class
     # nomsters: Mapped[]
+
+    def __repr__(self) -> str:
+        return (
+            f'Player(id={self.id!r}, unique_id={self.unique_id!r}, username={self.username!r}, '
+            f'nickname={self.nickname!r}, _medals={self._medals!r}, points={self.points!r}, _items={self._items!r})'
+        )
