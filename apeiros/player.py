924c57fa112be7a461d175fa3fbfef969458889b
Nyhilo <jacclowa@gmail.com>
2023-09-17 16:45:33 -0700

    Add image sanitization and update player identifier sanitization

--- apeiros/player.py
+++ apeiros/player.py
@@ -1,13 +1,14 @@
 from .game import db
 from .models import Player
-from .utilities.image import check_square
+from .utilities import image
 
 
 def create_player(
         unique_id: str | None,
         username: str | None,
         nickname: str | None,
-        player_token: bytes
+        player_token: bytes,
+        autocrop: bool = False
 ) -> None:
     '''
     !!Use check_square() before submitting an image through this method.!!
@@ -26,22 +27,11 @@ def create_player(
                                  identifying the user.
         player_token (bytes):   A square png. Maximum size of 200kb.
     '''
-    # Sanitization
-    if unique_id is None and username is None:
-        raise ValueError('Either a unique_id or a username must be given to create a new player.')
+    # Sanitize identifiers
+    unique_id, username, nickname = _handle_user_identifiers(unique_id, username, nickname)
 
-    if unique_id is not None and username is None and nickname is None:
-        raise ValueError('If a username is not given, a nickname must be provided to identify the player.')
-
-    if unique_id is None and username is not None:
-        unique_id = username
-
-    if nickname is None and username is not None:
-        nickname = username
-
-    pixels_off, _ = check_square(player_token)
-    if pixels_off != 0:
-        raise ValueError('Given player token image is not square.')
+    # Sanitize player token image
+    player_token = _handle_player_token(player_token, autocrop)
 
     player = Player(
         unique_id=unique_id,
@@ -78,3 +68,38 @@ def get_player_name(unique_id: str) -> str:
         return player.nickname
 
     return player.username
+
+
+# Written with Lily's help :)
+def _handle_user_identifiers(unique_id: str, username: str, nickname: str) -> (str, str, str):
+    has_unique_id = unique_id is not None
+    has_username = username is not None
+    has_nickname = nickname is not None
+
+    match (has_unique_id, has_username, has_nickname):
+        case (False, False, False):
+            raise ValueError('Bro you didn\'t even give any ids. Why do you think those params are there?')
+        case (_, False, False):
+            raise ValueError('If a username is not given, a nickname must be provided to identify the player.')
+        case (False, False, _):
+            raise ValueError('Either a unique_id or a username must be given to create a new player.')
+        case (False, True, _):
+            unique_id = username
+        case (_, True, False):
+            nickname = username
+
+    return (unique_id, username, nickname)
+
+
+def _handle_player_token(token: bytes, autocrop: bool = False) -> bytes:
+    # We'll just convert the image just to make sure
+    token = image.convert_png(token)
+
+    pixels_off, _ = image.check_square(token)
+    if pixels_off > 0 and not autocrop:
+        raise ValueError('Given player token image is not square.')
+
+    if autocrop:
+        return image.autocrop(token)
+
+    return token
