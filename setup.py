25a0cdebeec8822bf9ccc5cb1752ebd8fcd3e889
Nyhilo <jacclowa@gmail.com>
2023-09-17 16:48:36 -0700

    Version bump to 0.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='apeiros',
-    version='0.1.6',
+    version='0.1.7',
     description='A framework for the game Apeiros, an infinite nomic discord game',
     author='Nyhilo',
     url='https://github.com/Nyhilo/Apeiros-Game',
