4a6c90d8bebde2a5fb913a67518d55871ff0939b
Nyhilo <jacclowa@gmail.com>
2024-04-26 16:00:57 -0700

    Add created_at global property on all objects

--- apeiros/models/base_model.py
+++ apeiros/models/base_model.py
@@ -1,5 +1,17 @@
-from sqlalchemy.orm import DeclarativeBase
+import datetime
+
+from typing_extensions import Annotated
+
+from sqlalchemy import func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+timestamp = Annotated[
+    datetime.datetime,
+    mapped_column(nullable=False, server_default=func.CURRENT_TIMESTAMP()),
+]
 
 
 class Base(DeclarativeBase):
+    created_at: Mapped[timestamp]
     pass
